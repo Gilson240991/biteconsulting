b1f0f497b4a8c1332cbb5664126e33adc1d5a715
Gilson240991 <gilson.aguicarb@gmail.com>
2017-11-15 00:31:07 -0500

    ultimas modificaciones xD

--- index.php
+++ index.php
@@ -1,33 +1,5 @@
-<!DOCTYPE html>
-<html dir="ltr" lang="en-US">
-<head>
+<?php 	include "assets/header.php"; ?>
 
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<meta name="author" content="SemiColonWeb" />
-
-	<link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico">
-
-	<!-- Stylesheets
-	============================================= -->
-	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
-	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
-	<link rel="stylesheet" href="css/style.css" type="text/css" />
-	<link rel="stylesheet" href="css/swiper.css" type="text/css" />
-	<link rel="stylesheet" href="css/dark.css" type="text/css" />
-	<link rel="stylesheet" href="css/font-icons.css" type="text/css" />
-	<link rel="stylesheet" href="css/animate.css" type="text/css" />
-	<link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
-
-	<link rel="stylesheet" href="css/responsive.css" type="text/css" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-
-	<!-- Document Title
-	============================================= -->
-	<title>Inicio | BITE Consulting</title>
-
-</head>
-
-<body class="stretched sticky-responsive-pagemenu">
 
 	<!-- Document Wrapper
 	============================================= -->
@@ -46,26 +18,26 @@
 					<!-- Logo
 					============================================= -->
 					<div id="logo">
-						<a href="index.html" class="standard-logo" data-dark-logo="images/logo-dark.png"><img src="images/logo/BITE.png" alt="Canvas Logo"></a>
-						<a href="index.html" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="images/logo/BITE.png" alt="Canvas Logo"></a>
+						<a href="index.php" class="standard-logo" data-dark-logo="images/logo-dark.png"><img src="images/logo/BITE.png" alt="Canvas Logo"></a>
+						<a href="index.php" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="images/logo/BITE.png" alt="Canvas Logo"></a>
 					</div><!-- #logo end -->
 
 					<!-- Primary Navigation
 					============================================= -->
-					<nav id="primary-menu">
+					<nav id="primary-menu" class="one-page-menu">
 
 						<ul>
-							<li class="current"><a href="http://localhost/bite-consulting/prueba.php"><div>Inicio</div></a></li>
+							<li class="current"><a href="index.php"><div>Inicio</div></a></li>
 							
-							<li><a href="#" data-href='#nosotros'><div>Nosotros</div></a></li>
+							<li><a href="#" data-href="#nosotros"><div>Nosotros</div></a></li>
 
-							<li class="mega-menu"><a href="#"><div>Servicios</div></a></li>
+							<li class="mega-menu"><a href="#" data-href="#servicios"><div>Servicios</div></a></li>
 
-							<li class="mega-menu"><a href="#"><div>Portafolio</div></a></li>
+							<li class="mega-menu"><a href="#" data-href="#portafolio"><div>Portafolio</div></a></li>
 
-							<li class="mega-menu"><a href="#"><div>El Equipo</div></a></li>
+							<li class="mega-menu"><a href="#" data-href="#equipo"><div>Equipo</div></a></li>
 
-							<li class="mega-menu"><a href="#"><div>Contáctanos</div></a></li>
+							<li class="mega-menu"><a href="#" data-href="#contactanos"><div>Contáctanos</div></a></li>
 						</ul>
 
 						<!-- Top Search
@@ -73,7 +45,7 @@
 						<div id="top-search">
 							<a href="#" id="top-search-trigger"><i class="icon-search3"></i><i class="icon-line-cross"></i></a>
 							<form action="search.html" method="get">
-								<input type="text" name="q" class="form-control" value="" placeholder="Type &amp; Hit Enter..">
+								<input type="text" name="q" class="form-control" value="" placeholder="BUSCAR">
 							</form>
 						</div><!-- #top-search end -->
 
@@ -226,10 +198,10 @@
 
 				</section>
 
-				<section id="section-work" class="page-section topmargin-lg">
+				<section id="portafolio" class="page-section topmargin-lg">
 
 					<div class="heading-block center">
-						<h2>Our Works</h2>
+						<h2>Portafolio</h2>
 						<span>Some of the Awesome Projects we've worked on.</span>
 					</div>
 
@@ -444,10 +416,10 @@
 
 				</section>
 
-				<section id="section-team" class="page-section topmargin-lg">
+				<section id="equipo" class="page-section topmargin-lg">
 
 					<div class="heading-block center">
-						<h2>Our Team</h2>
+						<h2>Nuestro Equipo</h2>
 						<span>People who have contributed enormously to our Company.</span>
 					</div>
 
@@ -603,10 +575,10 @@
 
 				</section>
 
-				<section id="section-services" class="page-section topmargin-lg">
+				<section id="servicios" class="page-section topmargin-lg">
 
 					<div class="heading-block center bottommargin-lg">
-						<h2>Services</h2>
+						<h2>Servicios</h2>
 						<span>List of some features included in Canvas.</span>
 					</div>
 
@@ -714,127 +686,6 @@
 
 				</section>
 
-				<section id="section-pricing" class="page-section topmargin-lg">
-
-					<div class="heading-block center">
-						<h2>Pricing</h2>
-						<span>We offer Flexible Pricing Options.</span>
-					</div>
-
-					<div class="container clearfix">
-
-						<div class="pricing bottommargin clearfix">
-
-							<div class="col-md-3" data-animate="fadeInLeft">
-
-								<div class="pricing-box">
-									<div class="pricing-title">
-										<h3>Starter</h3>
-									</div>
-									<div class="pricing-price">
-										<span class="price-unit">&euro;</span>7<span class="price-tenure">/mo</span>
-									</div>
-									<div class="pricing-features">
-										<ul>
-											<li><strong>Full</strong> Access</li>
-											<li><i class="icon-code"></i> Source Files</li>
-											<li><strong>100</strong> User Accounts</li>
-											<li><strong>1 Year</strong> License</li>
-											<li>Phone &amp; Email Support</li>
-										</ul>
-									</div>
-									<div class="pricing-action">
-										<a href="#" class="btn btn-danger btn-block btn-lg">Sign Up</a>
-									</div>
-								</div>
-
-							</div>
-
-							<div class="col-md-3" data-animate="fadeInDown"  data-delay="250">
-
-								<div class="pricing-box best-price">
-									<div class="pricing-title">
-										<h3>Professional</h3>
-										<span>Most Popular</span>
-									</div>
-									<div class="pricing-price">
-										<span class="price-unit">&euro;</span>12<span class="price-tenure">/mo</span>
-									</div>
-									<div class="pricing-features">
-										<ul>
-											<li><strong>Full</strong> Access</li>
-											<li><i class="icon-code"></i> Source Files</li>
-											<li><strong>1000</strong> User Accounts</li>
-											<li><strong>2 Years</strong> License</li>
-											<li><i class="icon-star3"></i>
-											<i class="icon-star3"></i>
-											<i class="icon-star3"></i>
-											<i class="icon-star3"></i>
-											<i class="icon-star3"></i></li>
-										</ul>
-									</div>
-									<div class="pricing-action">
-										<a href="#" class="btn btn-danger btn-block btn-lg bgcolor border-color">Sign Up</a>
-									</div>
-								</div>
-
-							</div>
-
-							<div class="col-md-3" data-animate="fadeInUp" data-delay="500">
-
-								<div class="pricing-box">
-									<div class="pricing-title">
-										<h3>Business</h3>
-									</div>
-									<div class="pricing-price">
-										<span class="price-unit">&euro;</span>19<span class="price-tenure">/mo</span>
-									</div>
-									<div class="pricing-features">
-										<ul>
-											<li><strong>Full</strong> Access</li>
-											<li><i class="icon-code"></i> Source Files</li>
-											<li><strong>500</strong> User Accounts</li>
-											<li><strong>3 Years</strong> License</li>
-											<li>Phone &amp; Email Support</li>
-										</ul>
-									</div>
-									<div class="pricing-action">
-										<a href="#" class="btn btn-danger btn-block btn-lg">Sign Up</a>
-									</div>
-								</div>
-
-							</div>
-
-							<div class="col-md-3" data-animate="fadeInRight" data-delay="250">
-
-								<div class="pricing-box">
-									<div class="pricing-title">
-										<h3>Enterprise</h3>
-									</div>
-									<div class="pricing-price">
-										<span class="price-unit">&euro;</span>29<span class="price-tenure">/mo</span>
-									</div>
-									<div class="pricing-features">
-										<ul>
-											<li><strong>Full</strong> Access</li>
-											<li><i class="icon-code"></i> Source Files</li>
-											<li><strong>1000</strong> User Accounts</li>
-											<li><strong>5 Years</strong> License</li>
-											<li>Phone &amp; Email Support</li>
-										</ul>
-									</div>
-									<div class="pricing-action">
-										<a href="#" class="btn btn-danger btn-block btn-lg">Sign Up</a>
-									</div>
-								</div>
-
-							</div>
-
-						</div>
-
-					</div>
-
-				</section>
 
 				<section id="section-testimonials" class="page-section section parallax dark" style="background-image: url('images/about/me-parallax.jpg'); padding: 200px 0;" data-stellar-background-ratio="0.3">
 
@@ -889,7 +740,7 @@
 
 				</section>
 
-				<section id="section-contact" class="page-section">
+				<section id="contactanos" class="page-section">
 
 					<div class="heading-block title-center">
 						<h2>Ponte en contacto con nosotros</h2>
@@ -1028,252 +879,4 @@
 
 		</section><!-- #content end -->
 
-		<!-- Footer
-		============================================= -->
-		<footer id="footer" class="dark">
-
-			<div class="container">
-
-				<!-- Footer Widgets
-				============================================= -->
-				<div class="footer-widgets-wrap clearfix">
-
-					<div class="col_two_third">
-
-						<div class="col_one_third">
-
-							<div class="widget clearfix">
-
-								<img src="images/footer-widget-logo.png" alt="" class="footer-logo">
-
-								<p>We believe in <strong>Simple</strong>, <strong>Creative</strong> &amp; <strong>Flexible</strong> Design Standards.</p>
-
-								<div style="background: url('images/world-map.png') no-repeat center center; background-size: 100%;">
-									<address>
-										<strong>Lima - Perú:</strong><br>
-										Av. Santa Rosa, MZ W  LT 23<br>
-										San Martin de Porres<br>
-									</address>
-									<abbr title="CellPhone Number"><strong>CellPhone:</strong></abbr> (51) 959134676<br>
-									<abbr title="Phone"><strong>Phone:</strong></abbr> (51) 01 575 2849<br>
-									<abbr title="Email Address"><strong>Email:</strong></abbr> info@bite-consulting.com
-								</div>
-
-							</div>
-
-						</div>
-
-						<div class="col_one_third">
-
-							<div class="widget widget_links clearfix">
-
-								<h4>Blogroll</h4>
-
-								<ul>
-									<li><a href="http://codex.wordpress.org/">Documentation</a></li>
-									<li><a href="http://wordpress.org/support/forum/requests-and-feedback">Feedback</a></li>
-									<li><a href="http://wordpress.org/extend/plugins/">Plugins</a></li>
-									<li><a href="http://wordpress.org/support/">Support Forums</a></li>
-									<li><a href="http://wordpress.org/extend/themes/">Themes</a></li>
-									<li><a href="http://wordpress.org/news/">WordPress Blog</a></li>
-									<li><a href="http://planet.wordpress.org/">WordPress Planet</a></li>
-								</ul>
-
-							</div>
-
-						</div>
-
-						<div class="col_one_third col_last">
-
-							<div class="widget clearfix">
-								<h4>Recent Posts</h4>
-
-								<div id="post-list-footer">
-									<div class="spost clearfix">
-										<div class="entry-c">
-											<div class="entry-title">
-												<h4><a href="#">Lorem ipsum dolor sit amet, consectetur</a></h4>
-											</div>
-											<ul class="entry-meta">
-												<li>10th July 2014</li>
-											</ul>
-										</div>
-									</div>
-
-									<div class="spost clearfix">
-										<div class="entry-c">
-											<div class="entry-title">
-												<h4><a href="#">Elit Assumenda vel amet dolorum quasi</a></h4>
-											</div>
-											<ul class="entry-meta">
-												<li>10th July 2014</li>
-											</ul>
-										</div>
-									</div>
-
-									<div class="spost clearfix">
-										<div class="entry-c">
-											<div class="entry-title">
-												<h4><a href="#">Debitis nihil placeat, illum est nisi</a></h4>
-											</div>
-											<ul class="entry-meta">
-												<li>10th July 2014</li>
-											</ul>
-										</div>
-									</div>
-								</div>
-							</div>
-
-						</div>
-
-					</div>
-
-					<div class="col_one_third col_last">
-
-						<div class="widget clearfix" style="margin-bottom: -20px;">
-
-							<div class="row">
-
-								<div class="col-md-6 bottommargin-sm">
-									<div class="counter counter-small"><span data-from="50" data-to="15065421" data-refresh-interval="80" data-speed="3000" data-comma="true"></span></div>
-									<h5 class="nobottommargin">Total Downloads</h5>
-								</div>
-
-								<div class="col-md-6 bottommargin-sm">
-									<div class="counter counter-small"><span data-from="100" data-to="18465" data-refresh-interval="50" data-speed="2000" data-comma="true"></span></div>
-									<h5 class="nobottommargin">Clients</h5>
-								</div>
-
-							</div>
-
-						</div>
-
-						<div class="widget subscribe-widget clearfix">
-							<h5><strong>Subscribe</strong> to Our Newsletter to get Important News, Amazing Offers &amp; Inside Scoops:</h5>
-							<div class="widget-subscribe-form-result"></div>
-							<form id="widget-subscribe-form" action="include/subscribe.php" role="form" method="post" class="nobottommargin">
-								<div class="input-group divcenter">
-									<span class="input-group-addon"><i class="icon-email2"></i></span>
-									<input type="email" id="widget-subscribe-form-email" name="widget-subscribe-form-email" class="form-control required email" placeholder="Enter your Email">
-									<span class="input-group-btn">
-										<button class="btn btn-success" type="submit">Subscribe</button>
-									</span>
-								</div>
-							</form>
-						</div>
-
-						<div class="widget clearfix" style="margin-bottom: -20px;">
-
-							<div class="row">
-
-								<div class="col-md-6 clearfix bottommargin-sm">
-									<a href="#" class="social-icon si-dark si-colored si-facebook nobottommargin" style="margin-right: 10px;">
-										<i class="icon-facebook"></i>
-										<i class="icon-facebook"></i>
-									</a>
-									<a href="#"><small style="display: block; margin-top: 3px;"><strong>Me gusta</strong><br>on Facebook</small></a>
-								</div>
-								<div class="col-md-6 clearfix">
-									<a href="#" class="social-icon si-dark si-colored si-rss nobottommargin" style="margin-right: 10px;">
-										<i class="icon-rss"></i>
-										<i class="icon-rss"></i>
-									</a>
-									<a href="#"><small style="display: block; margin-top: 3px;"><strong>Subscribe</strong><br>to RSS Feeds</small></a>
-								</div>
-
-							</div>
-
-						</div>
-
-					</div>
-
-				</div><!-- .footer-widgets-wrap end -->
-
-			</div>
-
-			<!-- Copyrights
-			============================================= -->
-			<div id="copyrights">
-
-
-
-				<div class="container clearfix">
-
-
-					
-					<div class="siderbar center">
-						
-
-						<div class="clear"></div>
-
-						<i class="icon-envelope2"></i> info@bite-consulting.com <span class="middot">&middot;</span> <i class="icon-phone3"></i> +51 - 959134676<span class="middot">&middot;</span> <i class="icon-skype2"></i> BITEconsulting<br>
-
-                    <div class="col_full">
-					    All Rights Reserved by BITE Consulting<br>
-
-					<div class="col_full">
-						Copyrights &copy; 2017 <br>
-
-						
-					</div>
-				    </div>
-					</div>
-
-
-				</div>
-
-
-			</div><!-- #copyrights end -->
-
-		</footer><!-- #footer end -->
-
-	</div><!-- #wrapper end -->
-
-	<!-- Go To Top
-	============================================= -->
-	<div id="gotoTop" class="icon-angle-up"></div>
-
-	<!-- External JavaScripts
-	============================================= -->
-	<script type="text/javascript" src="js/jquery.js"></script>
-	<script type="text/javascript" src="js/plugins.js"></script>
-
-	<!-- Footer Scripts
-	============================================= -->
-	<script type="text/javascript" src="js/functions.js"></script>
-
-	<script type="text/javascript" src="https://maps.google.com/maps/api/js?key=AIzaSyDMxJ92oBkSnVNHFX3R8XhtYQPEgk1_IiI"></script>
-	<script type="text/javascript" src="js/jquery.gmap.js"></script>
-
-	<script type="text/javascript">
-
-		jQuery('#google-map').gMap({
-			address: 'Melbourne, Australia',
-			maptype: 'ROADMAP',
-			zoom: 14,
-			markers: [
-				{
-					address: "Melbourne, Australia",
-					html: '<div style="width: 300px;"><h4 style="margin-bottom: 8px;">Hi, we\'re <span>Envato</span></h4><p class="nobottommargin">Our mission is to help people to <strong>earn</strong> and to <strong>learn</strong> online. We operate <strong>marketplaces</strong> where hundreds of thousands of people buy and sell digital goods every day, and a network of educational blogs where millions learn <strong>creative skills</strong>.</p></div>',
-					icon: {
-						image: "images/icons/map-icon-red.png",
-						iconsize: [32, 39],
-						iconanchor: [32,39]
-					}
-				}
-			],
-			doubleclickzoom: false,
-			controls: {
-				panControl: true,
-				zoomControl: true,
-				mapTypeControl: true,
-				scaleControl: false,
-				streetViewControl: false,
-				overviewMapControl: false
-			}
-
-		});
-	</script>
-
-</body>
-</html>
+		<?php 	include "assets/footer.php" ?>
